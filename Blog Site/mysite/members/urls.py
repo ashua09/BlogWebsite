51b4f6e12d58265ce6f6133f728ccb05cd851623
Ashish Chaudhary <64960841+ashua09@users.noreply.github.com>
2022-09-16 12:08:27 +0530

    Update urls.py

--- Blog Site/mysite/members/urls.py
+++ Blog Site/mysite/members/urls.py
@@ -9,8 +9,6 @@ urlpatterns = [
     
     path('register/',views.UserRegister.as_view(),name='register'),
     path('edit_profile/',views.UserEditProfile.as_view(),name='edit_profile'),   
-    # path('password/',auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'),name='password_change'),
-    #can also do this way
     path('password/',views.PasswordChangeView.as_view(template_name='registration/password_change.html'),name='password_change'),
     path('pswrd_success/',views.password_success,name='pswrd_success'),
     path('<int:pk>/profile_page/',views.ShowProfilePage.as_view(),name='profile_page'),
